b5f8fd80e635c2d8b9933ba84292d28069ad8e32
René Rafael <rrafael@groupsolver.com>
2023-04-15 15:41:21 +0200

    Added eslint formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,6 +12,14 @@ return {
       },
       setup = {
         eslint = function()
+          require("lazyvim.util").on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+
           vim.api.nvim_create_autocmd("BufWritePre", {
             callback = function(event)
               if require("lspconfig.util").get_active_client_by_name(event.buf, "eslint") then
