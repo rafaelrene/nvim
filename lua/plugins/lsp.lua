489b796ce02ba8ba56b1d8b8db5f3a56fb371b30
René Rafael <skreciprodukcia@gmail.com>
2023-06-22 12:46:37 +0200

    feat(lazy): support go

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,40 +11,40 @@ return {
             workingDirectory = { mode = "auto" },
           },
         },
-        -- gopls = {
-        --   settings = {
-        --     gopls = {
-        --       semanticTokens = true,
-        --       completeUnimported = true,
-        --       usePlaceholders = true,
-        --       analyses = {
-        --         unusedparams = true,
-        --       },
-        --     },
-        --   },
-        -- },
+        gopls = {
+          settings = {
+            gopls = {
+              semanticTokens = true,
+              completeUnimported = true,
+              usePlaceholders = true,
+              analyses = {
+                unusedparams = true,
+              },
+            },
+          },
+        },
       },
       setup = {
-        -- gopls = function()
-        --   -- workaround for gopls not supporting semantictokensprovider
-        --   -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-        --   require("lazyvim.util").on_attach(function(client, _)
-        --     if client.name == "gopls" then
-        --       if not client.server_capabilities.semanticTokensProvider then
-        --         local semantic = client.config.capabilities.textDocument.semanticTokens
-        --         client.server_capabilities.semanticTokensProvider = {
-        --           full = true,
-        --           legend = {
-        --             tokenTypes = semantic.tokenTypes,
-        --             tokenModifiers = semantic.tokenModifiers,
-        --           },
-        --           range = true,
-        --         }
-        --       end
-        --     end
-        --   end)
-        --   -- end workaround
-        -- end,
+        gopls = function()
+          -- workaround for gopls not supporting semantictokensprovider
+          -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+          require("lazyvim.util").on_attach(function(client, _)
+            if client.name == "gopls" then
+              if not client.server_capabilities.semanticTokensProvider then
+                local semantic = client.config.capabilities.textDocument.semanticTokens
+                client.server_capabilities.semanticTokensProvider = {
+                  full = true,
+                  legend = {
+                    tokenTypes = semantic.tokenTypes,
+                    tokenModifiers = semantic.tokenModifiers,
+                  },
+                  range = true,
+                }
+              end
+            end
+          end)
+          -- end workaround
+        end,
         eslint = function()
           require("lazyvim.util").on_attach(function(client)
             if client.name == "eslint" then
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,19 +3,22 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       -- Add required parsers
-      table.insert(opts.ensure_installed, "css")
-      table.insert(opts.ensure_installed, "graphql")
-      table.insert(opts.ensure_installed, "html")
-      table.insert(opts.ensure_installed, "jsdoc")
-      table.insert(opts.ensure_installed, "json")
-      table.insert(opts.ensure_installed, "lua")
-      table.insert(opts.ensure_installed, "rust")
-      table.insert(opts.ensure_installed, "scss")
-      table.insert(opts.ensure_installed, "typescript")
-      -- table.insert(opts.ensure_installed, "go")
-      -- table.insert(opts.ensure_installed, "gomod")
-      -- table.insert(opts.ensure_installed, "gowork")
-      -- table.insert(opts.ensure_installed, "gosum")
+      vim.list_extend(opts.ensure_installed, {
+        "css",
+        "graphql",
+        "html",
+        "jsdoc",
+        "json",
+        "lua",
+        "rust",
+        "scss",
+        "typescript",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      })
+
       -- Disable hightlighing on big files
       opts.highlight.disable = function()
         return vim.b.large_buf
