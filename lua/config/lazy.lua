5c349eed88a838d6a2e9d295feacffefa20418b0
René Rafael <skreciprodukcia@gmail.com>
2023-07-13 19:16:28 +0200

    feat(colorscheme): switched to catpuccin with transparent background

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     -- import/override with your plugins
     { import = "plugins" },
   },
