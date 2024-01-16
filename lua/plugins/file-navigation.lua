29f41fce9d3b7287b9364fdc5d6419dd31538f4d
René Rafael <skreciprodukcia@gmail.com>
2024-01-16 15:55:39 +0100

    fix(file-nav): Turns out `plus` functions weren't the issue.

--- lua/plugins/file-navigation.lua
+++ lua/plugins/file-navigation.lua
@@ -2,10 +2,8 @@ return {
   "echasnovski/mini.files",
   opts = {
     mappings = {
-      go_in = "L",
-      go_out = "H",
-      go_in_plus = "",
-      go_out_plus = "",
+      go_in = "",
+      go_out = "",
     },
     windows = {
       width_preview = 100,
