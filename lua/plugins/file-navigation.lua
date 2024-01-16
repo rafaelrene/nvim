80a2865d1e5ce9516161777672f1fc60cab3959b
René Rafael <skreciprodukcia@gmail.com>
2024-01-16 15:52:57 +0100

    fix(file-nav): Fix mini.files navigation

--- lua/plugins/file-navigation.lua
+++ lua/plugins/file-navigation.lua
@@ -2,8 +2,10 @@ return {
   "echasnovski/mini.files",
   opts = {
     mappings = {
-      go_in = "",
-      go_out = "",
+      go_in = "L",
+      go_out = "H",
+      go_in_plus = "",
+      go_out_plus = "",
     },
     windows = {
       width_preview = 100,
